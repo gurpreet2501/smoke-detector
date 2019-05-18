8518cbe95dc1ca80c5f06f7fe9f9a6dfe8ab0841
Gurpreet Singh <gurpreet2501@gmail.com>
2019-05-18 22:49:30 +0530

    Changes in class

--- database/migrations/2019_05_18_162142_add_m_otion_detectors_tbl.php
+++ database/migrations/2019_05_18_162142_add_m_otion_detectors_tbl.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddMOtionDetectorsTbl extends Migration
+class AddMOtionDetectorsTblMigSecond extends Migration
 {
     /**
      * Run the migrations.
