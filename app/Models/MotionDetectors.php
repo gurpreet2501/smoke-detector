b7278041f46cdd3d1eb5c7ce5c2a485c4ed79690
Gurpreet Singh <gurpreet2501@gmail.com>
2019-05-20 21:21:55 +0530

    motion detector api modified

--- app/Models/MotionDetectors.php
+++ app/Models/MotionDetectors.php
@@ -7,7 +7,7 @@ class MotionDetectors extends Model
 {
     protected $table = 'sdetect_motion_detectors';
 
-    protected $fillable = ['machine_id','serial','name','motion_detector_status','motion_detector_button_status' ,'user_id'];
+    protected $fillable = ['machine_id','serial','name','status','user_id'];
 
 	  protected $validationMessages = [];
 
