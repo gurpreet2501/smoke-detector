a984467b39171fe0daf2f16882d5ad932fb6d8df
Gurpreet Singh <gurpreet2501@gmail.com>
2019-06-08 11:44:37 +0530

    key added

--- config/notifications.php
+++ config/notifications.php
@@ -6,7 +6,7 @@ return [
     'push' => [
         'apple' => [
             'env' => AppleService::SANDBOX_MODE, //PRODUCTION_MODE
-            'proCert'       => 'Certificates_ySkolar.pem',//'ProPushDev.pem',
+            'proCert'       => 'AuthKey_CA7HY748WQ.p8',//'ProPushDev.pem',
         ],
         'android' => [
             'customer_app'  => ['key' => 'AAAA7yL_BYo:APA91bEUK9Lqk8dCv2g8hDvmqKx9Y6u_IYZxWC5VE2iP8vdftlFqnAUXooJ2M5E2w-KfpnFjD8aca8U0VWiVpq98CotB_unUxViFjm6bCAA8gTqAmvLDlBblm5s9-hdqFLZKxSAEm-k2'],
